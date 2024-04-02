4494666dc02c082e41ada399b44ca98de56ad7d8
Youss9f <165741477+Youss9f@users.noreply.github.com>
2024-04-02 02:03:27 +0000

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+function guess_files {
+    local num_files=$(ls -1 | wc -l)
+    local guess=-1
+
+    while [[ $guess -ne $num_files ]]; do
+        read -p "How many files are in the current directory? " guess
+
+        if [[ $guess -lt $num_files ]]; then
+            echo "Too low!"
+        elif [[ $guess -gt $num_files ]]; then
+            echo "Too high!"
+        fi
+    done
+
+    echo "Congratulations! You guessed correctly."
+}
+
+guess_files
